58322b7fb198abce7a1e03f7785f2d1ca3bb7c37
wgwilt <wgwilt84@gmail.com>
2016-12-12 16:22:19 +1000

    Update site test

---
+++ src/AandPManagement/Data/Migrations/20161212044440_AzureMigration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AandPManagement.Data.Migrations
+{
+    public partial class AzureMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/AandPManagement/Migrations/20161212044514_AzureMigration.Designer.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using AandPManagement.Data;
+
+namespace AandPManagement.Migrations
+{
+    [DbContext(typeof(ProjectContext))]
+    [Migration("20161212044514_AzureMigration")]
+    partial class AzureMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("AandPManagement.Models.Asset", b =>
+                {
+                    b.Property<int>("AssetID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("AssetAllocated");
+
+                    b.Property<DateTime?>("AssetAnualTestDate");
+
+                    b.Property<DateTime?>("AssetCOCDate");
+
+                    b.Property<string>("AssetConnections");
+
+                    b.Property<string>("AssetDescription")
+                        .IsRequired();
+
+                    b.Property<string>("AssetDimensions");
+
+                    b.Property<DateTime?>("AssetLiftDate");
+
+                    b.Property<string>("AssetLocation");
+
+                    b.Property<DateTime?>("AssetMPITestDate");
+
+                    b.Property<DateTime?>("AssetMajorTestDate");
+
+                    b.Property<bool>("AssetPreJobCheck");
+
+                    b.Property<string>("AssetPreJobCompletedBy");
+
+                    b.Property<int?>("AssetPressureRating");
+
+                    b.Property<string>("AssetSerialNumber")
+                        .IsRequired();
+
+                    b.Property<DateTime?>("AssetVisualTestDate");
+
+                    b.Property<int?>("AssetWeight");
+
+                    b.Property<string>("COC");
+
+                    b.Property<int?>("ProjectID");
+
+                    b.HasKey("AssetID");
+
+                    b.HasIndex("ProjectID");
+
+                    b.ToTable("Asset");
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.ClientCompany", b =>
+                {
+                    b.Property<int>("ClientCompanyID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Address");
+
+                    b.Property<string>("Name")
+                        .IsRequired();
+
+                    b.HasKey("ClientCompanyID");
+
+                    b.ToTable("ClientCompany");
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.Personnel", b =>
+                {
+                    b.Property<int>("PersonnelID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Email");
+
+                    b.Property<string>("Name")
+                        .IsRequired();
+
+                    b.Property<string>("PhoneNumber");
+
+                    b.Property<string>("Position")
+                        .IsRequired();
+
+                    b.Property<int?>("ProjectID");
+
+                    b.HasKey("PersonnelID");
+
+                    b.HasIndex("ProjectID");
+
+                    b.ToTable("Personnel");
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.Project", b =>
+                {
+                    b.Property<int>("ProjectID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("ClientCompanyID");
+
+                    b.Property<string>("ProjectClient")
+                        .IsRequired();
+
+                    b.Property<bool>("ProjectCompleted");
+
+                    b.Property<DateTime?>("ProjectCompletedDate");
+
+                    b.Property<string>("ProjectLocation")
+                        .IsRequired();
+
+                    b.Property<string>("ProjectNumber");
+
+                    b.Property<DateTime>("ProjectStartDate");
+
+                    b.Property<string>("ProjectTitle")
+                        .IsRequired();
+
+                    b.HasKey("ProjectID");
+
+                    b.HasIndex("ClientCompanyID");
+
+                    b.ToTable("Project");
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.ProjectTask", b =>
+                {
+                    b.Property<int>("ProjectTaskID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("ProjectID");
+
+                    b.Property<bool>("TaskComplete");
+
+                    b.Property<DateTime?>("TaskCompletedDate");
+
+                    b.Property<string>("TaskDescription")
+                        .IsRequired();
+
+                    b.Property<DateTime?>("TaskSetCompletionDate");
+
+                    b.Property<DateTime>("TaskSetDate");
+
+                    b.Property<string>("TaskTitle")
+                        .IsRequired();
+
+                    b.HasKey("ProjectTaskID");
+
+                    b.HasIndex("ProjectID");
+
+                    b.ToTable("ProjectTask");
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.Training", b =>
+                {
+                    b.Property<int>("TrainingID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateOfTraining");
+
+                    b.Property<int>("PersonnelID");
+
+                    b.Property<string>("TypeTraining")
+                        .IsRequired();
+
+                    b.HasKey("TrainingID");
+
+                    b.HasIndex("PersonnelID");
+
+                    b.ToTable("Training");
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.Asset", b =>
+                {
+                    b.HasOne("AandPManagement.Models.Project", "Project")
+                        .WithMany("Assets")
+                        .HasForeignKey("ProjectID");
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.Personnel", b =>
+                {
+                    b.HasOne("AandPManagement.Models.Project", "Project")
+                        .WithMany("Crew")
+                        .HasForeignKey("ProjectID");
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.Project", b =>
+                {
+                    b.HasOne("AandPManagement.Models.ClientCompany", "ClientCompany")
+                        .WithMany("Projects")
+                        .HasForeignKey("ClientCompanyID");
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.ProjectTask", b =>
+                {
+                    b.HasOne("AandPManagement.Models.Project", "Project")
+                        .WithMany("ProjectTasks")
+                        .HasForeignKey("ProjectID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("AandPManagement.Models.Training", b =>
+                {
+                    b.HasOne("AandPManagement.Models.Personnel", "Personnel")
+                        .WithMany("Training")
+                        .HasForeignKey("PersonnelID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
